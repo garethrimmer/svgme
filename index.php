5423b6cd120670007d0dcdef2b391e9871434dbf
Gareth Rimmer <gareth@garethrimmer.co.uk>
2016-03-11 12:01:41 +0000

    Added some mobile meta tags.

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="utf-8">
   <title>svgme</title>
+  
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   
   <meta name="description" content="">
   	<!-- OG Meta -->
