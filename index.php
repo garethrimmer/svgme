15603cf8c928a18139de6357d2c4d1a64c024193
Gareth Rimmer <gareth@garethrimmer.co.uk>
2016-03-11 10:38:28 +0000

    Added links

--- index.php
+++ index.php
@@ -3,7 +3,17 @@
 <head>
   <meta charset="utf-8">
   <title>svgme</title>
+  
   <meta name="description" content="">
+  	<!-- OG Meta -->
+	<meta property="og:site_name" content="Quadraphorge" />
+	<meta property="og:title" content="A quick and dirty svg animation experiment" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="http://www.quadraphorge.co.uk" />
+	<meta property="og:image" content="http://www.quadraphorge.co.uk/gforce.png" />
+	<meta property="og:description" content="Quick and dirty svg animation experiment" />
+	<!-- /OG Meta-->
+	
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <link rel="stylesheet" href="styles/css/styles.css">
@@ -16,7 +26,19 @@
 	<?php// include("includes/test-02.svg"); ?>
 	
 	<?php include("includes/gsvg.svg"); ?>
+	
+	<p><a href="http://garethrimmer.co.uk">garethrimmer.co.uk</a> – <a href="https://github.com/garethrimmer/svgme">GitHub</a></p>
 
 <script src="js/min/scripts-min.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-1670159-28', 'auto');
+  ga('send', 'pageview');
+
+</script>
 </body>
 </html>
